f101808ca394a340db8918a21a1325e5642d4967
Tim <tcarmich@hotmail.com>
2014-08-13 00:05:08 +0100

    Added Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    # required for Heroku ** Needs to be updated TODO
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
