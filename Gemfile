deb180fd74448a255a0f309ad04da4e9b231f6df
Tim <tcarmich@hotmail.com>
2014-08-12 17:27:20 +0100

    Bootstrap AddedChanges

--- Gemfile
+++ Gemfile
@@ -29,6 +29,9 @@ gem 'spring',        group: :development
 # Bootstrap
 gem 'bootstrap-sass'
 
+#Vendor Prefixes
+gem 'autoprefixer-rails'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "pages/home"
-  get "tim" => "pages#home"
+  get "about" => "pages#about"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
