5bdfd4bc5e249f126e3d6706313c04a69af2989f
Tim <tcarmich@hotmail.com>
2014-08-14 14:24:31 +0100

    masonry config

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ActiveRecord::Base
      belongs_to :user
-     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :default_url => "public/images/missing.png"}
 		validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
 
